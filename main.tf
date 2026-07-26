7ec9ab967b0408357cc2cce3e34c79a17100cb9c
pradip Bhattacharjya <pradipbhattacharjya1997@gmail.com>
2026-07-26 17:21:14 +0530

    First Commit to feature/dev Branch

--- main.tf
+++ main.tf
@@ -13,9 +13,9 @@ provider "aws" {
 }
 
 resource "aws_instance" "my_instance" {
-  ami                    = var.ami_id
-  instance_type          = var.instance_type
-  key_name               = "my-key"
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  # key_name               = "my-key"
   vpc_security_group_ids = [aws_security_group.web_sg.id]
   user_data              = file("nginx.sh")
   metadata_options {
